a785d2ac980d2b2afb1d1437589dc8fb41114c49
odan <odan3240@gmail.com>
2019-10-26 17:16:29 +0900

    chore(backend): setup

---
+++ packages/backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  plugins: [
+    "@typescript-eslint"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off"
+  }
+}
