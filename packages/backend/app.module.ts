ad8811d42d13c9b893dfbdcc38a084c3a4213525
odan <odan3240@gmail.com>
2019-11-07 23:51:21 +0900

    chore(app): add mysql

--- packages/backend/app.module.ts
+++ packages/backend/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TodoModule } from './src/todo/todo.module';
 
+const typeOrmModule = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3002,
+  username: 'webapp-template',
+  password: 'webapp-template',
+  database: 'webapp-template',
+  entities: [],
+  synchronize: true
+});
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, typeOrmModule],
   controllers: [],
   providers: []
 })
